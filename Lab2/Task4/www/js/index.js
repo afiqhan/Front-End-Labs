7c7b19a02ae483f4a5a2520edefac95ae6f4be42
afiqhan <121393948+afiqhan@users.noreply.github.com>
2023-06-29 00:36:58 +0800

    Add files via upload

---
+++ Lab2/Task4/www/js/index.js
@@ -0,0 +1,60 @@
+
+document.addEventListener("deviceready", onDeviceReady, false);
+
+function onDeviceReady() {
+    // Cordova is now initialized. Have fun!
+    document.getElementById("getPosition").addEventListener("click", getPosition);
+document.getElementById("watchPosition").addEventListener("click", watchPosition);
+
+    console.log("Running cordova-" + cordova.platformId + "@" + cordova.version);
+    document.getElementById("deviceready").classList.add("ready");
+}
+
+// to get the position
+function getPosition() {
+    var options = {
+       enableHighAccuracy: true,
+       maximumAge: 3600000
+    }
+    var watchID = navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
+   // if success
+    function onSuccess(position) {
+       alert('Latitude: '          + position.coords.latitude          + '\n' +
+          'Longitude: '         + position.coords.longitude         + '\n' +
+          'Altitude: '          + position.coords.altitude          + '\n' +
+          'Accuracy: '          + position.coords.accuracy          + '\n' +
+          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+          'Heading: '           + position.coords.heading           + '\n' +
+          'Speed: '             + position.coords.speed             + '\n' +
+          'Timestamp: '         + position.timestamp                + '\n');
+    };
+    // if error
+    function onError(error) {
+       alert('code: '    + error.code    + '\n' + 'message: ' + error.message + '\n');
+    }
+ }
+ // same applied in the above method
+ function watchPosition() {
+    var options = {
+       maximumAge: 3600000,
+       timeout: 3000,
+       enableHighAccuracy: true,
+    }
+    var watchID = navigator.geolocation.watchPosition(onSuccess, onError, options);
+ 
+    function onSuccess(position) {
+       alert('Latitude: '          + position.coords.latitude          + '\n' +
+          'Longitude: '         + position.coords.longitude         + '\n' +
+          'Altitude: '          + position.coords.altitude          + '\n' +
+          'Accuracy: '          + position.coords.accuracy          + '\n' +
+          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+          'Heading: '           + position.coords.heading           + '\n' +
+          'Speed: '             + position.coords.speed             + '\n' +
+          'Timestamp: '         + position.timestamp                + '\n');
+    };
+ 
+    function onError(error) {
+       alert('code: '    + error.code    + '\n' +'message: ' + error.message + '\n');
+    }
+ }
+
